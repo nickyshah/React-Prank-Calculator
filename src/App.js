d7fdae3cd58c18ae7b028ddd1b224898b17db73d
Nicky <nickshah534@gmail.com>
2023-09-25 14:13:48 +1000

    updated

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ if(val === "."){
 setStrToDisplay(strToDisplay + val)
   
 }
- 
+
 
 const total = () => {
 
@@ -176,36 +176,38 @@ const playAudio = () => {
   return (
     <div className="wrapper">
         <div className="calculator">
-        <div className="display" > {strToDisplay}</div>
-          {
-          btns.map((item, i) => <Button 
-          key={i}  
-          cls = {item.cls} 
-          label={item.label} 
-          handleOnChange ={ handleOnChange} />)
-            
-          }
-            {/* <div class="display">0.00</div> */}
-            
-            {/* <div class="btn btn-ac">AC</div>
-            <div class="btn btn-c">C</div>
-            <div class="btn btn-per">%</div>
-            <div class="btn btn-divide">/</div>
-            <div class="btn btn-7">7</div>
-            <div class="btn btn-8">8</div>
-            <div class="btn btn-9">9</div>
-            <div class="btn btn-x">*</div>
-            <div class="btn btn-4">4</div>
-            <div class="btn btn-5">5</div>
-            <div class="btn btn-6">6</div>
-            <div class="btn btn-minus">-</div>
-            <div class="btn btn-1">1</div>
-            <div class="btn btn-2">2</div>
-            <div class="btn btn-3">3</div>
-            <div class="btn btn-plus">+</div>
-            <div class="btn btn-0">0</div>
-            <div class="btn btn-dot">.</div>
-            <div class="btn btn-equal">=</div> */}
+            <div className= {isPrank ? "display prank" : "display"}>
+               {strToDisplay || "0.00"}
+            </div>
+            {
+            btns.map((item, i) => <Button 
+            key={i}  
+            cls = {item.cls} 
+            label={item.label} 
+            handleOnChange ={ handleOnChange} />)
+              
+            }
+              {/* <div class="display">0.00</div> */}
+              
+              {/* <div class="btn btn-ac">AC</div>
+              <div class="btn btn-c">C</div>
+              <div class="btn btn-per">%</div>
+              <div class="btn btn-divide">/</div>
+              <div class="btn btn-7">7</div>
+              <div class="btn btn-8">8</div>
+              <div class="btn btn-9">9</div>
+              <div class="btn btn-x">*</div>
+              <div class="btn btn-4">4</div>
+              <div class="btn btn-5">5</div>
+              <div class="btn btn-6">6</div>
+              <div class="btn btn-minus">-</div>
+              <div class="btn btn-1">1</div>
+              <div class="btn btn-2">2</div>
+              <div class="btn btn-3">3</div>
+              <div class="btn btn-plus">+</div>
+              <div class="btn btn-0">0</div>
+              <div class="btn btn-dot">.</div>
+              <div class="btn btn-equal">=</div> */}
             
         </div>
 
